01eb9035ec6a5d398a90a787bfde4215b2a69326
Juma <gerald.juma@student.moringaschool.com>
2025-03-10 23:20:12 +0300

    done

---
+++ index.js
@@ -0,0 +1,11 @@
+let currentUser = "John Doe";
+
+let welcomeMessage = "Welcome to Flatbook, " + currentUser + "!";
+
+let excitedWelcomeMessage = "WELCOME TO FLATBOOK, " + currentUser.toUpperCase() + "!";
+
+let shortGreeting = "Welcome, " + currentUser.charAt(0) + "!";
+
+console.log(welcomeMessage);
+console.log(excitedWelcomeMessage);
+console.log(shortGreeting);
